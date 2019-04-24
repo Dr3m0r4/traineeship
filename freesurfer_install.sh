6cd9f90e030191fca2a065745b40babf59714610
dr3m <julien.feuillas@laposte.net>
2019-04-24 13:00:32 +0200

    update install

--- freesurfer_install.sh
+++ freesurfer_install.sh
@@ -8,10 +8,6 @@ source ~/.bashrc
 
 rm freesurfer-Linux-centos6_x86_64-stable-pub-v6.0.0.tar.gz
 
-wget https://mail.uca.fr/service/home/~/?auth=co&loc=fr&id=5084&part=2&disp=a
-
-mv license.txt $FREESURFER_HOME
-
 mkdir freesurfer.tmp && cd freesurfer.tmp
 curl https://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/dev/freesurfer-linux-centos6_x86_64-dev.tar.gz -o fsdev.tar.gz
 tar -xzvf fsdev.tar.gz
