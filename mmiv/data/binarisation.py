33246dc9ca4b46a62dc9f9c3a53d5e4e96611135
dr3m <julien.feuillas@laposte.net>
2019-05-09 17:58:36 +0200

    commit du to. 09. mai 17:58:36 +0200 2019

---
+++ mmiv/data/binarisation.py
@@ -0,0 +1,24 @@
+import os
+import tensorflow as tf
+import numpy as np
+
+import nibabel as nib
+
+path = os.path.join("labels")
+liste = os.listdir(path)
+cpt = 0
+
+for name in liste:
+	cpt+=1
+	file = os.path.join(path, name)
+	file_bin = os.path.join('labels_bin', name)
+	img = nib.load(file)
+	data = img.get_data().copy()
+	ind = np.where(data > 0)
+	data_bin = np.zeros(data.shape)
+	data_bin[ind]=1
+	print(name," | iter : ", cpt)
+	img_bin = nib.Nifti1Image(data_bin, img.affine)
+	img_bin.header.set_data_dtype(np.int16)
+	img_bin.header.set_xyzt_units('mm')
+	nib.save(img_bin,file_bin)
