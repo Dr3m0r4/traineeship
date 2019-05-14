126b8ef6889daa5e3051338e94882e5cc61a40ed
Dr3m0r4 <dr3m@Oblivion>
2019-05-14 14:52:21 +0200

    presentation

--- loss_function/lr_modification.py
+++ loss_function/lr_modification.py
@@ -2,7 +2,7 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-f = lambda x,y: max*(np.cos(x/500.0*np.pi+y*np.pi)+1)/(x/1500.0+1)
+f = lambda x,y: 1e-2*(np.cos(x/500.0*np.pi+y*np.pi)+1)/(x/1500.0+1)
 
 space = range(20001)
 y = 0
